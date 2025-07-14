9c139a4425f13d0ae3fcafc55f294326ca1f23fc
Prakash <pandeyprakash799@gmail.com>
2025-07-14 12:14:25 +0000

    datatypes

---
+++ 01_basics/01_dataTypes.js
@@ -0,0 +1,28 @@
+"use strict"; //teat all JS code as newer version
+
+//alert(3+3); we are using node js, not browser
+
+console.log(3+3); 
+console.log("Prakash");
+
+
+let name = "Prakash";
+let age = 35;
+let isLoggedIn = false;
+
+//number => 2 to power 53
+//bigint
+//string => ""
+//boolean => true, false
+//null => standalone value
+//undefined => 
+//symbol => uniqueness
+
+//object
+
+console.log(typeof undefined);
+console.log(typeof null) //object
+
+
+
+
---
+++ 01_basics/01_variables.js
@@ -0,0 +1,12 @@
+const accountId = 144553
+let accoutEmail = "prakash@gmail.com"
+var accountPassword = "12345"
+accountCity = "Gorakhpur"
+let accountState;
+
+/*
+Prefer not to use var
+because of scpe issue in block and function scope
+*/
+
+console.table([accountId,accoutEmail,accountPassword,accountCity,accountState]);
