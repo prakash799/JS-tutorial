364759f89d38644cef96d6661233ab18935245e7
Prakash <pandeyprakash799@gmail.com>
2025-07-14 13:08:50 +0000

    converstion of data types

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,17 @@
+let score = "prakash"; 
+
+let valueInNumber = Number(score);
+
+//console.log(typeof valueInNumber);
+
+//console.log(valueInNumber);
+
+
+
+let isLoggedIn = null;
+
+console.log(isLoggedIn);
+
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+console.log(booleanIsLoggedIn);
+
