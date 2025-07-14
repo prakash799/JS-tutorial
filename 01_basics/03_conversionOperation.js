c32ba875ffde68aa215af32fba32febcdbc48a19
Prakash <pandeyprakash799@gmail.com>
2025-07-14 13:26:41 +0000

    conversion part 2

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -10,8 +10,27 @@ let valueInNumber = Number(score);
 
 let isLoggedIn = null;
 
-console.log(isLoggedIn);
+//console.log(isLoggedIn);
 
 let booleanIsLoggedIn = Boolean(isLoggedIn);
-console.log(booleanIsLoggedIn);
+//console.log(booleanIsLoggedIn);
+
+
+
+//***************************** Operations ***********************************
+let value = 3;
+let negValue = -value;
+//console.log(negValue)
+
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2/2)
+// console.log("4%2 ->"+4%2)
+// console.log("2%4 -> "+2%4)
+// console.log(2*2)
+
+let str1 = "hello"
+let str2 = " prakash"
+let str3 = str1+str2
+// console.log(str3)
 
